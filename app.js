8eaafbd5b990bcbde2799c3441ba67f8ba870bc1
Jayesh <jayesh.wadhe@moreyeahs.in>
2023-01-10 12:19:52 +0530

    j

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const authroute = require('./routes/user_routes')
 const bodyparser = require('body-parser')
 const swaggerUi=require('swagger-ui-express');
 const swaggerDocument=require('./swagger.json');
+const port = process.env.PORT || 3000
+var j = []
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(bodyparser.json());
@@ -17,7 +19,7 @@ require('./config/db')
 
  
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log(`
     ################################################
     🛡️  Server listening On Port: 3000 🛡️
